82580fa6967945620c4ff1574115f66b50628aa3
Rod <letr.rod@gmail.com>
2022-08-01 06:45:42 +0300

    Extended description of program execution.

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,12 @@ int main() {
     std::cout << "Starting" << std::endl;
     std::string toy_name = "Test";
     SharedToyPtr test = make_shared_toy(toy_name);
+    std::cout << "Toy created." << std::endl;
 
     ToyOwner owner1(test);
+    std::cout << "Owner 1 created." << std::endl;
     ToyOwner owner2(test);
+    std::cout << "Owner 2 created." << std::endl;
 
-    std::cout << "Ending" << std::endl;
+    std::cout << "Ending, Exiting program." << std::endl;
 }
